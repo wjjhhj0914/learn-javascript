058d22b0316c998adedad170b26c85645e393c93
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-10 13:37:25 +0900

    [학습] JavaScript 변수 이름 작성 규칙 - 공백, 숫자, 하이픈, 예약어 사용 X - 의미 있는 이름 작성 권장

--- learn/05-variables.js
+++ learn/05-variables.js
@@ -1,12 +1,29 @@
 // 변수(Variables)
 
-console.log(4 + 27);
+// console.log(4 + 27);
 
-let 처음_가진_사과의_갯수 = 4;
+// let 처음_가진_사과의_갯수 = 4;
+let initialApples = 4;
+console.log('Initial Apples : ', initialApples);
 
-let 추가로_구매한_사과의_갯수 = 27;
-console.log('추가로_구매한_사과의_갯수', 추가로_구매한_사과의_갯수);
+// let 추가로_구매한_사과의_갯수 = 27;
+// console.log('추가로_구매한_사과의_갯수', 추가로_구매한_사과의_갯수);
 
-let 현재_내가_가진_사과의_갯수 =
-  처음_가진_사과의_갯수 + 추가로_구매한_사과의_갯수;
-console.log('현재_내가_가진_사과의_갯수', 현재_내가_가진_사과의_갯수);
+// let 현재_내가_가진_사과의_갯수 =
+//   처음_가진_사과의_갯수 + 추가로_구매한_사과의_갯수;
+// console.log('현재_내가_가진_사과의_갯수', 현재_내가_가진_사과의_갯수);
+
+// let initial_apples = 4 // snakeCase
+// console.log('initial_apples =', initial_apples)
+let $initialApples = 4; // camelCase
+console.log('$initialApples =', $initialApples);
+
+// 잘못된 변수 이름Add commentMore actions
+// 공백(' ')은 안돼요!
+// let initial apples [❌]
+// 첫글자가 숫자 안돼요!
+// let 4people = '4명의 친구들' [❌]
+// 하이픈(-) 기호 안돼요!
+// let dark-impact = '다크 임팩트' [❌]
+// 예약어(예: if, for, while, function, ...) 안돼요!
+// let return = '회귀' [❌]
