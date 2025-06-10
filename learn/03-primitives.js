b45119d440b1210efe8438fc863d9d104e26a1e8
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-10 11:07:19 +0900

    [학습] Number 데이터 타입
    - 덧셈, 뺄셈, 곱셈, 나눗셈, 나머지 연산
    - JS 정수의 안전 범위 (음수와 양수 사이 약 9000조)

--- learn/03-primitives.js
+++ learn/03-primitives.js
@@ -26,3 +26,29 @@
 // String Concatenation (문자열 연결)
 1 + 1; // 2
 '1' + '1'; // 11
+
+// ------------------------------------------------
+// Number
+console.log(10000);
+console.log(10_000);
+console.log(10.9843);
+console.log(0.49393);
+console.log();
+
+// 산술 연산
+// 덧셈 (add):       +
+// 뺄셈 (subtract):  -
+// 곱셈 (multiply):  *
+// 나눗셈 (divide):   /
+// 나머지 (modulus):  %
+
+console.log(10000000 + 3999382);
+console.log(10000000 - 3999382);
+console.log(10000000 * 3999382);
+console.log(10000000 / 3999382);
+console.log(10000000 + 3999382);
+console.log(10000000 % 6);
+
+// JavaScript 숫자(정수)의 안전 범위 표현
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER);
