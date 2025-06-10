f5fe264c6136e8d5bc06fff74d14f79bedac51e4
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-10 14:41:24 +0900

    [학습] 변수 vs. 상수
    - let, var 변수
    - const 상수

--- learn/05-variables.js
+++ learn/05-variables.js
@@ -8,6 +8,12 @@ console.log('Initial Apples : ', initialApples);
 
 // let 추가로_구매한_사과의_갯수 = 27;
 // console.log('추가로_구매한_사과의_갯수', 추가로_구매한_사과의_갯수);
+let addBuyApples; // 기본값 (undefined)
+// let addBuyApples = undefined; (암묵적)
+console.log('addBuyApples', addBuyApples); // undefined
+
+addBuyApples = 27;
+console.log('addBuyApples', addBuyApples); // 27
 
 // let 현재_내가_가진_사과의_갯수 =
 //   처음_가진_사과의_갯수 + 추가로_구매한_사과의_갯수;
@@ -27,3 +33,34 @@ console.log('$initialApples =', $initialApples);
 // let dark-impact = '다크 임팩트' [❌]
 // 예약어(예: if, for, while, function, ...) 안돼요!
 // let return = '회귀' [❌]
+
+// ----------------------------------------------------
+// let 변수 vs. var 변수
+
+// 블록 스코프(block scope)
+{
+  var myName;
+  console.log(1, myName);
+
+  myName = 'yamoo9';
+  console.log(2, myName);
+}
+
+myName = 'y9';
+console.log(3, myName);
+
+{
+  let myName2;
+  console.log(1, myName2);
+
+  myName2 = 'yamoo9';
+  console.log(2, myName2);
+}
+
+/*var*/ myName2 = 'y9';
+console.log(3, myName2);
+
+// ----------------------------------------------------
+// let 변수 vs. const 변수(상수, constant)
+// - 변수 = 시간의 흐름에 따라 값이 변경됨.
+// - 상수 = 처음 선언 시, 할당된 값이 이후 변경되지 않음.
