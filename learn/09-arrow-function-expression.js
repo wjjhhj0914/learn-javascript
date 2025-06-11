dc93fc4d1f2bfa32feb9b5029909e026116fb5b6
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-11 15:14:47 +0900

    [학습] JavaScript 함수를 만드는 방법 3가지
    - 1. 함수 선언
    - 2. 함수 표현식
    - 3. 화살표 함수 표현식

---
+++ learn/09-arrow-function-expression.js
@@ -0,0 +1,22 @@
+// 하살표 함수 표현식 (Arrow Function Expression)
+// ------------------------------------------------------
+
+// 함수를 만드는 방법 3가지
+
+// 1. 함수 선언
+function showMeTheMoney() {
+  console.log('Show me the money.');
+}
+showMeTheMoney();
+
+// 2. 함수 표현식
+let lookAtMe = function () {
+  console.log('Look at me!');
+};
+lookAtMe();
+
+// 3. 화살표 함수 표현식
+let readABook = () => {
+  console.log('Read a book!');
+};
+readABook();
