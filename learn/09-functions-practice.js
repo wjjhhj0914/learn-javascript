c10a16e2e4dccce419def208be2b702da5d6bcc5
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-11 15:08:03 +0900

    [학습] 커스텀 함수
    - 함수 만들기 실습

--- learn/09-functions-practice.js
+++ learn/09-functions-practice.js
@@ -20,8 +20,72 @@ console.log(pxToRem(189));
 function remToPx(rem) {
   let px = rem * 16 + 'px';
   return px;
-  // return num * 16 + 'px';
+  // return rem * 16 + 'px';
 }
 
 console.log(remToPx(1));
 console.log(remToPx(1.5));
+
+// 4. 주문한 피자 가격 계산 함수 만들기
+function priceOfPizza(price, amount) {
+  let sum = price * amount + '원';
+  return sum;
+}
+
+console.log(priceOfPizza(28900, 2));
+
+// 5. 주차 요금 계산 함수 만들기
+const HOUR_PARKING_FEE = 1000;
+
+let calcParkingFee = function (time) {
+  let calculator = time * HOUR_PARKING_FEE + '원';
+  return calculator;
+};
+
+console.log(calcParkingFee(1.5));
+console.log(calcParkingFee(5));
+
+// 6. 짝수인지 확인하는 함수 만들기
+function isEvenNumber(num) {
+  let evenOrOdd = num % 2 == 0;
+
+  return evenOrOdd;
+}
+
+console.log(isEvenNumber(187091));
+
+// 7. 생일 축하 메시지 출력 함수 만들기
+function isYourBirthdayToday(name) {
+  return name + '님의 생일을 축하합니다! 🎉';
+}
+
+console.log(isYourBirthdayToday('장효정'));
+console.log(isYourBirthdayToday('Charlie'));
+
+// 8. 올해 남은 날짜 계산 함수 만들기
+const NUMBER_OF_DAYS_OF_YEAR = 365;
+
+function restOfDate(dateOfToday) {
+  let calc = NUMBER_OF_DAYS_OF_YEAR - dateOfToday;
+  return calc;
+}
+
+console.log(restOfDate(120));
+
+// 9. 세금 포함 가격 계산 함수 만들기
+const TAX_RATIO = 0.1;
+
+let calcPriceWithTax = function (producePrice) {
+  return producePrice + producePrice * TAX_RATIO;
+};
+
+console.log(calcPriceWithTax(9000));
+
+// 10. 나이가 성인인지 판별하는 함수 만들기
+const ENTRY_AGE = 19;
+
+let isAdult = function (yourAge) {
+  return yourAge > ENTRY_AGE;
+};
+
+console.log(isAdult(19));
