b50924510aa4a6e45bf3f8a3d1fa6580c9b43ac1
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-13 16:10:25 +0900

    [학습] Web APIs 문서 요소 선택
    - Document.querySelector(selector)

---
+++ learn/14-document-object-model.js
@@ -0,0 +1,30 @@
+// ------------------------------------
+// 📌 DOM (문서 객체 모델)
+// - 문서 요소 선택(접근)하는 함수들 (Web APIs)
+// ------------------------------------
+
+// document.getElementsByTagName(tagName) <- 데이터 값은 string
+// document.getElementById()
+
+// console.log(document.getElementsByTagName('h1'));
+// const emailInput = document.getElementById('useremail'); // 데이터 타입 객체
+// console.log(emailInput);
+
+// Document.getElementsByName(name)
+// console.log(document.getElementsByName('email'));
+
+// Document.getElementByClassName(className)
+// const items = document.getElementsByClassName('item');
+// console.log(items);
+
+const elementById = document.querySelector('#useremail');
+console.log(elementById);
+
+const elementByName = document.querySelector('[name="email"]');
+console.log(elementByName);
+
+const firstElementByClassName = document.querySelector('.item');
+console.log(firstElementByClassName);
+
+const items = document.querySelectorAll('.item');
+console.log();
