602f10d576cbf0e49cc86838479aa4309cb5151c
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-13 11:25:55 +0900

    [학습] fromHexadecimal 함수
    - 16진 -> 10진수 변경 반환

--- learn/14-toString-parseInt.js
+++ learn/14-toString-parseInt.js
@@ -26,7 +26,10 @@ const toHexadecimal = n => n.toString(16);
 console.log(toHexadecimal(255));
 
 // 10진수를 2진수 또는 16진수로 변경하는 함수 작성
-// ...
+const calcRadix = (n, base = 16) => n.toString(base);
+
+console.log(calcRadix(100));
+console.log(calcRadix(100, 2));
 
 // --------------------------------------------------------------------------
 // 📌 parseInt(string, radix?)
@@ -36,16 +39,32 @@ console.log(toHexadecimal(255));
 // --------------------------------------------------------------------------
 
 // 단위 제거
-// ...
+console.log(parseInt('42px'));
+console.log(parseFloat('1.625rem'));
+console.log(parseInt('277mm'));
 
 // 단위 제거 함수 작성
-// ...
+const removeUnit = unit => parseFloat(unit, 10); // 기본이 10진수
+// parseFloat는 16진수로 못 바꿔줌. parseInt를 써야 함.
+
+console.log(removeUnit('40px'));
+console.log(removeUnit('1.625rem'));
 
 // 2진수 문자열 -> 10진수 변경
-// ...
+console.log(parseInt('10', 2));
+console.log(parseInt('100', 2));
 
 // 16진수 문자열 -> 10진수 변경
-// ...
+console.log(parseInt('00', 16));
+console.log(parseInt('80', 16));
+console.log(parseInt('a0', 16));
+console.log(parseInt('a1', 16));
+console.log(parseInt('ae', 16));
+console.log(parseInt('ff', 16));
 
 // 16진수 문자열을 10진수로 변경하는 함수 작성
-// ...
+const fromHexadecimal = hexValue => parseInt(hexValue, 16);
+
+console.log(fromHexadecimal('00'));
+console.log(fromHexadecimal('ae'));
+console.log(fromHexadecimal('ff'));
