987d77f660ccd059ad8bd17243a952a287c5e08d
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-16 14:23:13 +0900

    [학습] 이벤트 리스너 함수에 괄호를 왜 붙이지 말아야 하는지

--- learn/15-clickEventListener.js
+++ learn/15-clickEventListener.js
@@ -9,3 +9,38 @@ clickableButton.addEventListener('click', () => {
   // Event 객체에는 { x, y 좌표, target, type, pointerType, ... } 을 제공함.
 })
 
+// const myButton = document.querySelector('.click-button');
+// myButton.addEventListener('click', () => {
+//   console.log('Button clicked!!!');
+//   myButton.classList.toggle('another-class');
+// })
+
+/**
+ * const button = document.querySelector('button')
+
+function alertLog(e) {
+  console.log('clicked button')
+  button.classList.toggle('clicked')
+
+  // 명시적 반환
+  // return { name: '나는 반환값이다.' }
+  return () => {
+    console.log('나는 함수가 반환한 함수이다!')
+  }
+
+  // 암묵적으로 반환
+  // return undefined
+}
+
+// alertLog
+const result = alertLog()
+console.log(result) // undefined
+
+
+// 함수 이름만 참조
+// button.addEventListener('click', alertLog)
+
+// 함수 이름으로 호출(실행)
+// 이벤트 연결 (binding events)
+button.addEventListener('click', alertLog())
+ */
