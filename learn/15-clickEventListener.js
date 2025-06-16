baec1faf9242cc501309a60dd2bfc6ddcab3c36a
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-16 13:21:10 +0900

    [학습] 이벤트 객체 소개
    - 이벤트 리스너(함수)는 이벤트 객체를 매개변수로 전달 받음

--- learn/15-clickEventListener.js
+++ learn/15-clickEventListener.js
@@ -2,7 +2,10 @@ const clickableButton = document.querySelector('.clickable');
 const body = document.body;
 
 clickableButton.addEventListener('click', () => {
-  body.classList.add('is-active');
-  clickableButton.classList.add('is-active');
+  body.classList.toggle('is-active');
+  clickableButton.classList.toggle('is-active');
+
+  // addEventListener()에 전달된 함수(값), 즉 이벤트 리스너는 event 객체를 매개변수로 전달 받게 되어 있음.
+  // Event 객체에는 { x, y 좌표, target, type, pointerType, ... } 을 제공함.
 })
 
