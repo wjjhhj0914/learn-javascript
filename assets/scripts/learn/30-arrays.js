58511c1d2b0dc54fa3ca35ac7bbddaf10c822149
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-18 10:48:50 +0900

    [학습] JavaScript 배열(Array)
    - 리터럴 방식으로 배열 객체 생성
    - new Array 방식으로 배열 객체 생성 (new 키워드 안 써도 결과 동일)

---
+++ assets/scripts/learn/30-arrays.js
@@ -0,0 +1,9 @@
+// 배열(Array) 객체
+
+// 배열 리터럴로 배열 만들기 (배열 생성)
+const shoppingList = ['대파', '계란', '두부', '시금치', '양파', '케찹'];
+console.log(shoppingList, shoppingList.length);
+
+// 리터럴 방식 말고, 정석적인 방법으로 배열 객체 생성
+const cart = new Array('one', 'two', 'three', 'four', 'five');
+console.log(cart, cart.length);
