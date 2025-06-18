90a8a3aad1b9a3d1291f381e6a0805ef42169dae
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-18 14:29:52 +0900

    [실습] indexOf() vs. findIndex()
    - 기본 타입의 인덱스를 찾을 땐 indexOf()
    - 객체 타입의 인덱스를 찾을 땐 findIndex()

--- assets/scripts/learn/31-array-methods.js
+++ assets/scripts/learn/31-array-methods.js
@@ -21,3 +21,41 @@ const hasItem = function (array, fruitName) {
   return array.indexOf(fruitName) > -1;
 }
 console.log(hasItem(fruitBasket, '패션후르츠'));
+
+// indexOf() 메서드 대신 includes() 메서드 활용
+console.log(fruitBasket.includes('워터멜론')); // true
+console.log(fruitBasket.includes('수박')); // false
+
+// 객체 타입(함수, 배열, 객체)은 indexOf()로 판별 가능하지 않음!
+// 대신 findIndex()
+const LIKELION_14TH = [
+  { name: '장효정' },
+  { name: '장영은' },
+]
+
+// 값이 있으면 해당 인덱스를 찾아줌 0
+// 값이 없으면 -1
+LIKELION_14TH.findIndex((student) => student.name === '장효정');
+
+// ------------------------------------------------------------
+// 배열에 항목 추가
+
+// 시작 부분에 항목 추가
+const animals = ['cat', 'dog', 'elephant'];
+animals.unshift('giraffe');
+console.log(animals);
+
+// 끝 부분에 항목 추가
+animals.push('kangaroo');
+animals.push('koala', 'dolphins', 'turtle');
+
+
+const nestedArray = [
+  [1, 2, 3],
+  ['one', 'two', 'three',],
+];
+
+// 배열 원소 내부에 기본 타입만 포함된 경우
+// .indexOf()
+console.log(nestedArray.indexOf([1, 2, 3])); // -1 : 못 찾음
+
