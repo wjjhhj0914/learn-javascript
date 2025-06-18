0c50b6c22f99c599b44e81e25d07e05f98c6101f
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-18 17:00:32 +0900

    [학습] 루프(반복)문
    - if vs. while

---
+++ assets/scripts/learn/33-loop.js
@@ -0,0 +1,31 @@
+// 루프(Loop)
+
+const numbers = [100, 99, 98, 97];
+console.log(numbers);
+
+/*
+let wantToDraw = true
+
+// 조건문
+// 조건이 참이면 블록 내부의 코드 한 번 수행
+if (wantToDraw) {
+  draw()
+}
+
+// 루프(반복)문 
+// 조건이 참이면 블록 내부의 코드를 조건이 거짓이 될 때까지 반복 수행
+
+let n = 0
+
+while (wantToDraw) {
+  draw()
+
+  // 조건을 거짓으로 만들께요! (멈춤)
+  // wantToDraw = !wantToDraw
+  if (n > 2) {
+    wantToDraw = false
+  }
+  
+  n += 1
+}
+*/
