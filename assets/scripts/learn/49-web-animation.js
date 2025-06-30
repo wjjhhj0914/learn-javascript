4e97fe9be0ec576ad26bfde6f0d715ed3583339d
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-30 15:10:47 +0900

    [학습 및 실습] CSS 트랜지션 트리거(with JS), CSS 트랜지션 타이밍 함수 + JS 트리거
    - CSS 커스텀 속성 활용
    - 개별 트랜지션 제어

--- assets/scripts/learn/49-web-animation.js
+++ assets/scripts/learn/49-web-animation.js
@@ -1,32 +1,43 @@
-// 🚀 웹 애니메이션
-// ----------------------------------------------------------------
+// --------------------------------------------------------------------------
+// 📌 웹 애니메이션
+// --------------------------------------------------------------------------
+// 1. CSS Transitions
+// 2. CSS Animations
+// 3. JavaScript Animations
+// --------------------------------------------------------------------------
 
-
-(() => {
-  const ballElement = document.querySelector('.ball');
-  const TRANSITION_CLASSNAME = 'is-transition';
-
-  // mouse click event + className toggling
-  // 마우스로 볼 요소를 클릭하면 활성 상태로 변경/이전 상태로 전환
+;(() => {
+  
+  const ballElement = document.querySelector('.ball')
+  // const TRANSITION_CLASSNAME = 'is-transition'
+  
+  // Mouse Click Event + className toggling
+  // 마우스로 볼(ball) 요소를 클릭하면 활성 상태로 전환
+  // 마우스로 볼(ball) 요소를 다시 클릭하면 이전 상태로 전환
   ballElement.addEventListener('click', (e) => {
-    e.currentTarget.classList.toggle(TRANSITION_CLASSNAME);
-    // const ball = e.currentTarget;
-    // if (ball.classList.contains(TRANSITION_CLASSNAME)) {
-    //   ball.classList.remove(TRANSITION_CLASSNAME);
-    // } else {
-    //   ball.classList.add(TRANSITION_CLASSNAME);
-    // }
-    
-  });
-  // ballElement.addEventListener('click', (e) => {
-  //   e.currentTarget.classList.remove(TRANSITION_CLASSNAME);
-  // })
 
-  // mouse enter/leave events
+    // style 속성을 사용해 속성의 상태 변경
+    // 트랜지션 트리거
+    const ball = e.currentTarget
+
+    ball.style.setProperty('--fill', '#e54d26')
+    ball.style.setProperty('--size', '100px')
+
+    // ball.classList.toggle(TRANSITION_CLASSNAME)
+    
+    // if (ball.classList.contains(TRANSITION_CLASSNAME)) {
+    //   ball.classList.remove(TRANSITION_CLASSNAME)
+    // } else {
+    //   ball.classList.add(TRANSITION_CLASSNAME)
+    // }
+  })
+
+  // Mouse Enter/Leave Events
   // ballElement.addEventListener('mouseenter', (e) => {
-  //   e.currentTarget.classList.add(TRANSITION_CLASSNAME);
+  //   e.currentTarget.classList.add(TRANSITION_CLASSNAME)
   // })
   // ballElement.addEventListener('mouseleave', (e) => {
-  //   e.currentTarget.classList.remove(TRANSITION_CLASSNAME);
+  //   e.currentTarget.classList.remove(TRANSITION_CLASSNAME)
   // })
-})();
+  
+})()
