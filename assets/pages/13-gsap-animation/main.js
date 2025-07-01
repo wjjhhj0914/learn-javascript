011fb2c43638cc779250f78fdd56acff4b792404
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-07-01 13:49:54 +0900

    [학습] GSAP 애니메이션 - 타임라인 소개
    - 스태거의 한계
    - 타임라인의 필요성

--- assets/pages/13-gsap-animation/main.js
+++ assets/pages/13-gsap-animation/main.js
@@ -1,4 +1,5 @@
-;(() =>{
+// GSAP - Getting Started
+; (() => {
 
   // Web Animations API
   function playground() {
@@ -158,76 +159,93 @@
   }, 500)
 })
 
-;(() => {
+// GSAP - Stagger
+  ; (() => {
 
-  function animate() {
+    function animate() {
 
-    // gsap.from('.blue', { y: 100, opacity: 0, duration: 1 })
-    // gsap.from('.yellow', { y: 100, opacity: 0, duration: 1, delay: 1 })
-    // gsap.from('.green', { y: 100, opacity: 0, duration: 1, delay: 2 })
+      // gsap.from('.blue', { y: 100, opacity: 0, duration: 1 })
+      // gsap.from('.yellow', { y: 100, opacity: 0, duration: 1, delay: 1 })
+      // gsap.from('.green', { y: 100, opacity: 0, duration: 1, delay: 2 })
 
-    gsap.from('.box', {
-      y: 100,
-      opacity: 0,
-      stagger: 0.1,
-      ease: 'power1.out',
-    })
-
-    // 이벤트 위임(Event Delegation)
-    document.querySelector('main')
-      .addEventListener('click', (e) => {
-        if (e.target.matches('.box')) {
-          gsap.to('.box', {
-            y: -100,
-            opacity: 0,
-            scale: 1.2,
-            // repeat: -1, // 1번, 5번, -1 무한
-
-            // stagger: 0.1,
-
-            stagger : {
-              each: 0.3,
-              repeat: 5,
-              yoyo: true,
-            },
-
-            // stagger: (index, target, /* list */) => {
-            //   // return index * 0.2 // 0, 0.2, 0.4
-            //   // return index * 0.4 // 0, 0.4, 0.8
-
-            //   if (target.matches('.green')) {
-            //     return 1.5
-            //   } else {
-            //     return index * 0.2
-            //   }
-            // },
-            ease: 'power4.out',
-
-            // callbacks
-            onStart: () => { console.log('start'); },
-            onUpdate: () => { console.log('update'); },
-            onComplete: () => { console.log('complete'); }
-          })
-        }
+      gsap.from('.box', {
+        y: 100,
+        opacity: 0,
+        stagger: 0.1,
+        ease: 'power1.out',
       })
 
+      // 이벤트 위임(Event Delegation)
+      document.querySelector('main')
+        .addEventListener('click', (e) => {
+          if (e.target.matches('.box')) {
+            gsap.to('.box', {
+              y: -100,
+              opacity: 0,
+              scale: 1.2,
+              // repeat: -1, // 1번, 5번, -1 무한
 
-    // 루프문 사용
-    // const boxes = document.querySelectorAll('.box')
-    // for (const box of boxes) {
-    //   box.addEventListener('click', () => {
-    //     gsap.to('.box', {
-    //       y: -100,
-    //       opacity: 0,
-    //       scale: 1.2,
-    //       stagger: 0.1,
-    //       ease: 'power4.out',
-    //     })
-    //   })
-    // }
+              // stagger: 0.1,
 
+              stagger: {
+                each: 0.3,
+                repeat: 5,
+                yoyo: true,
+              },
+
+              // stagger: (index, target, /* list */) => {
+              //   // return index * 0.2 // 0, 0.2, 0.4
+              //   // return index * 0.4 // 0, 0.4, 0.8
+
+              //   if (target.matches('.green')) {
+              //     return 1.5
+              //   } else {
+              //     return index * 0.2
+              //   }
+              // },
+              ease: 'power4.out',
+
+              // callbacks
+              onStart: () => { console.log('start'); },
+              onUpdate: () => { console.log('update'); },
+              onComplete: () => { console.log('complete'); }
+            })
+          }
+        })
+
+
+      // 루프문 사용
+      // const boxes = document.querySelectorAll('.box')
+      // for (const box of boxes) {
+      //   box.addEventListener('click', () => {
+      //     gsap.to('.box', {
+      //       y: -100,
+      //       opacity: 0,
+      //       scale: 1.2,
+      //       stagger: 0.1,
+      //       ease: 'power4.out',
+      //     })
+      //   })
+      // }
+
+    }
+
+    setTimeout(animate, 500)
+
+  });
+
+// GSAP - Timeline
+(() => {
+  function animate() {
+    gsap.to('.box', {
+      rotation: 360, duration: (index) => {
+        if (index === 0) {
+          return 2;
+        } else {
+          return 0.1;
+        }
+      }, stagger: 0.1 });
   }
 
-  setTimeout(animate, 500)
-
-})()
+  setTimeout(animate, 500);
+})();
