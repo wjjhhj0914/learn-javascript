c51ec2bbf6eba6df8443fbd57e4bbada2b0bdad8
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-07-02 15:26:09 +0900

    [학습] 유용한 JS 기능 - 논리 연산자 실습

--- assets/scripts/learn/51-logical-operator-expression.js
+++ assets/scripts/learn/51-logical-operator-expression.js
@@ -67,3 +67,48 @@
   }
 })();
 
+// 1. 실습
+// 1. 영화 관람 가능 여부 확인
+(() => {
+  // 다음 조건을 모두 만족해야 영화관에 입장할 수 있습니다
+  // - 18세 이상 관람가
+  // - 신분증 필
+  // canWatchMovie 함수 코드를 작성하세요. (논리 연산자 활용)
+  function canWatchMovie(person) {
+    let result;
+    // 논리 연산자
+    // 논리곱(&&): 첫 번째 거짓으로 평가되는 값을 찾아 반환.
+    // 논리합(||): 첫 번째 참으로 평가되는 값을 찾아 반환.
+
+    // result = (person.age >= 18 && person.idCard) && '관람 허용';
+    
+    person.age >= 18 && person.idCard ? result = '관람 허용' : result = '관람 불가';
+    return result;
+  }
+  
+  const results = [
+    canWatchMovie({ age: 21, idCard: true }), // 관람 허용 | 관람 허용
+    canWatchMovie({ age: 15, idCard: false }), // false | 관람 불가
+  ]
+  
+  console.log(results) // ['관람 허용', '관람 불가']
+});
+
+// 2. 실습
+// 2. 우산 챙겨야 할 지 결정
+(() => {
+  function needUmbrella(condition) {
+    if (condition.isRaining || condition.forecastRain) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  const results = [
+    needUmbrella({ isRaining: false, forecastRain: true }),
+    needUmbrella({ isRaining: false, forecastRain: false }),
+  ]
+  
+  console.log(results) // [true, false]
+})();
