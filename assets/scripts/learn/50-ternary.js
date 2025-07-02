763c5643c648e26094f23190a6570eeda2ebb2c2
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-07-02 13:31:34 +0900

    [학습] 유용한 JS 기능 - 문 vs. 식 - 문은 값을 반환하지 않음 - 식은 값을 반환함 - 함수는 값을 항상 반환함 - 설사 암묵적으로 undefined가 반환된더라도

--- assets/scripts/learn/50-ternary.js
+++ assets/scripts/learn/50-ternary.js
@@ -2,14 +2,35 @@
 // 3항 연산자 (표현)식 (Ternary)
 // ----------------------------------------------------------------
 
-{
-  const user = {
-    name: '조현주',
-    age: 23,
-    role: '손님',
-  }
+const user = {
+  name: '조현주',
+  age: 23,
+  role: '손님',
+};
 
+// Ternary
+(() => {
   user.role === '관리자'
     ? console.log('서비스의 모든 권한을 가지므로, 어떤 일이든 가능합니다.')
     : console.warn('서비스 관리자가 아니므로, 접근이 불가합니다.');
-} 
+});
+
+// Conditional Statement vs Ternary
+(() => {
+  {
+    let message = '';
+
+    // 문은 값을 반환하지 않는다.
+    if (user.role === '관리자') {
+      message = '서비스 관리자이므로, 어떤 일이든 가능합니다.';
+    } else {
+      message = '서비스 관리자가 아니므로, 접근이 불가합니다..';
+    }
+  }
+
+  // 조건식 (Conditional Statment)
+  let message = user.role === '관리자'
+    ? '서비스 관리자이므로, 어떤 일이든 가능합니다.'
+    : '서비스 관리자가 아니므로, 접근이 불가합니다..';
+  console.log(message);
+})();
